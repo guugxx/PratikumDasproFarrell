36128c3280170143200ac42b6fce81fd9925992f
Galih1004 <mochgalihputrapratama@gmail.com>
2025-09-25 11:36:26 +0700

    Galih menambahkan baris pertama

---
+++ Test14.java
@@ -0,0 +1,7 @@
+package PratikumDasproFarrell;
+
+public class Test14 {
+    public static void main(String[] args) {
+        System.out.println("Halo ini dari user galih : ini adalah contoh baris pertama");
+    }
+}
