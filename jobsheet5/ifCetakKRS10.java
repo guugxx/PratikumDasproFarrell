222a6135dcd9324bbf3bf4eb5b97a46e37d9b898
Farrell <farrellraissa4@gmail.com>
2025-10-02 00:00:19 +0700

    Tugas 1(real)

--- jobsheet5/ifCetakKRS10.java
+++ jobsheet5/ifCetakKRS10.java
@@ -3,18 +3,21 @@ import java.util.Scanner;
 public class ifCetakKRS10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("---Cetak KRS SIAKAD---");
-        System.out.println("Apakah UKT sudah lunas? (true/false): ");
+
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        } else {
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-        }
+        
+        String hasil = (uktLunas) 
+                        ? "Silakan cetak KRS Anda." 
+                        : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.";
+
+        System.out.println(hasil);
+
+        sc.close();
     }
 }
+
 
 
 
