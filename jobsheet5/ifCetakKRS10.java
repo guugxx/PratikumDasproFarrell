f405b85c60d36555ccd4240303914733825987f0
Farrell <farrellraissa4@gmail.com>
2025-10-01 14:06:28 +0700

    Percobaan 1

---
+++ jobsheet5/ifCetakKRS10.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ifCetakKRS10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("---Cetak KRS SIAKAD---");
+        System.out.println("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } 
+    }
+}
+
+
+//1. Karena uktLunas sudah boolean (true/false)
+//2. Tidak keluar pesan apa-apa, program langsung selesai
+//3. 
