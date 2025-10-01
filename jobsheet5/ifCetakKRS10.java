394e533bf98647014fa16dc92460977c058827b8
Farrell <farrellraissa4@gmail.com>
2025-10-01 14:50:00 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRS10.java
+++ jobsheet5/ifCetakKRS10.java
@@ -10,9 +10,12 @@ public class ifCetakKRS10 {
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        } 
+        } else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+        }
     }
 }
+
 
 
 //1. Karena uktLunas sudah boolean (true/false)
